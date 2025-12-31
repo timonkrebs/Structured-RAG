281ce904582f00fc0e490870556cb132b7f7af5f
timon <timon.jucker@fhnw.ch>
2025-12-31 01:51:43 +0100

    further improvements of the tag generation prompt

--- StructuredRAG.Api/Services/TagGenerationService.cs
+++ StructuredRAG.Api/Services/TagGenerationService.cs
@@ -99,7 +99,7 @@ public class TagGenerationService
         You must not use more than 3 words per tag!
         Instead of [""Nature and technology"", ...] or [""nature, technology"", ...] make multiple tags like [""nature"", ""technology"", ...].
         Do not split tags like [""team analysis"", ...] into [""team"", ""analysis"", ...] but keep it as [""team analysis"", ...]!
-        Do not use comma seperated words in tags. Instead of [""nature, technology"", ...] make multiple tags like [""nature"", ""technology"", ...]!
+        Do not use comma seperated words in tags. Instead of [""aesthetics, space, immersion"", ""experience, theory, design"", ""sensory, spatial, narrative""] make multiple tags like [""aesthetics"", ""space"", ""immersion"", ""sensory experience"", ""spatial theory"", ""design narrative"", ...]!
 
 Titel: '{entity.Name}'
 Content: '{entity.Content}'{existingTagsText}
